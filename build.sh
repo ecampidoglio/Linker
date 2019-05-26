6b8298adfd1204a48d63bef08d47a5d096c6f994
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2019-05-26 14:28:01 +0200

    Uses the Cake Tool in the Bash bootstrapper
    Instead of downloading Cake.CoreCLR, we use the Cake Tool instead,
    since it's clearly the way forward and we're already on .NET Core 2.1
    (the minimum version required to run custom .NET Core CLI Tools).

--- build.sh
+++ build.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 ########################################################################################
 # This is a Cake bootstrapper script for Linux and macOS using .NET Core 2.1
-# Inspired by
-# https://adamhathcock.blog/2017/07/12/net-core-on-circle-ci-2-0-using-docker-and-cake/
 ########################################################################################
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 CAKE_VERSION=0.33.0
 TOOLS_DIR=$SCRIPT_DIR/tools
-TOOLS_PROJ=$TOOLS_DIR/tools.csproj
-CAKE_DLL=$TOOLS_DIR/Cake.CoreCLR.$CAKE_VERSION/cake.coreclr/$CAKE_VERSION/Cake.dll
+CAKE_TOOL=$TOOLS_DIR/dotnet-cake
 
 # Make sure the tools folder exist
 if [ ! -d "$TOOLS_DIR" ]; then
   mkdir "$TOOLS_DIR"
 fi
 
-# Install Cake.CoreCLR
-if [ ! -f "$CAKE_DLL" ]; then
-    echo -e "\e[92mInstalling Cake $CAKE_VERSION for .NET Core in '$TOOLS_DIR'\e[0m"
-    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.1</TargetFramework></PropertyGroup></Project>" > $TOOLS_PROJ
-    dotnet add $TOOLS_PROJ package Cake.CoreCLR -v $CAKE_VERSION --package-directory $TOOLS_DIR/Cake.CoreCLR.$CAKE_VERSION
+# Install the Cake Tool locally
+if [ ! -f "$CAKE_TOOL" ]; then
+    echo -e "\033[92mInstalling 'cake.tool' $CAKE_VERSION in '$TOOLS_DIR'\033[0m"
+    dotnet tool install Cake.Tool --version $CAKE_VERSION --tool-path $TOOLS_DIR
 fi
 
 # Run the build script
-exec dotnet "$CAKE_DLL" "$@"
+exec "$CAKE_TOOL" "$@"
